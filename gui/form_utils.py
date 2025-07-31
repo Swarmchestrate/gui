cb9ef058eebc358a4059ee69013c2d3cbf0897ad
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-07-31 15:38:52 +0100

    Implement capacity form field generation

---
+++ gui/form_utils.py
@@ -0,0 +1,63 @@
+from django import forms
+
+
+class DefaultConfiguredField:
+    field_class = forms.CharField
+    widget_class = forms.Textarea
+
+    def __init__(
+            self,
+            field_properties: dict,
+            is_required: bool = False) -> None:
+        self.field_properties = field_properties
+        self.is_required = is_required
+
+    def get_widget_classes(self) -> list[str]:
+        return ['form-control']
+
+    def get_widget_kwargs(self) -> dict:
+        return {
+            'attrs': {
+                'class': ' '.join(self.get_widget_classes()),
+            }
+        }
+
+    def get_field_kwargs(self) -> dict:
+        kwargs = {
+            'required': self.is_required,
+            'widget': self.widget_class(**self.get_widget_kwargs()),
+        }
+        field_description = self.field_properties.get('description')
+        if not field_description:
+            return kwargs
+        kwargs.update({
+            'help_text': field_description,
+        })
+        return kwargs
+
+    @property
+    def field_instance(self) -> forms.Field:
+        return self.field_class(**self.get_field_kwargs())
+
+
+class ConfiguredBooleanField(DefaultConfiguredField):
+    field_class = forms.BooleanField
+    widget_class = forms.CheckboxInput
+
+    def get_widget_classes(self) -> list[str]:
+        return ['form-check-input']
+
+
+class ConfiguredCharField(DefaultConfiguredField):
+    field_class = forms.CharField
+    widget_class = forms.TextInput
+
+
+class ConfiguredDateField(DefaultConfiguredField):
+    field_class = forms.DateField
+    widget_class = forms.DateInput
+
+
+class ConfiguredIntegerField(DefaultConfiguredField):
+    field_class = forms.IntegerField
+    widget_class = forms.NumberInput
--- gui/forms.py
+++ gui/forms.py
@@ -1,5 +1,57 @@
+import os
 from django import forms
+from django.conf import settings
+from enum import Enum
+from prance import ResolvingParser
+
+from .form_utils import (
+    DefaultConfiguredField,
+    ConfiguredBooleanField,
+    ConfiguredCharField,
+    ConfiguredDateField,
+    ConfiguredIntegerField,
+)
+
+
+CAPACITY_PATH = '/model'
+
+
+class OpenApiFormat(Enum):
+    BOOLEAN = 'boolean'
+    CHARACTER_VARYING = 'character varying'
+    DATE = 'date'
+    INTEGER = 'integer'
+    UUID = 'uuid'
 
 
 class NewCapacityForm(forms.Form):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        parser = ResolvingParser(os.path.join(settings.BASE_DIR, 'swagger.yaml'))
+        post_parameters_schema = next(iter(parser.specification.get('paths').get(CAPACITY_PATH).get('post').get('parameters'))).get('schema')
+        required_field_names = post_parameters_schema.get('required')
+        fields = post_parameters_schema.get('properties')
+        for field_key, field_metadata in fields.items():
+            is_required = field_key in required_field_names
+            new_field = self._get_configured_field(field_metadata, is_required=is_required)
+            self.fields.update({
+                field_key: new_field,
+            })
+
+    def _get_configured_field(self, field_metadata: dict, is_required: bool = False):
+        field_format = field_metadata.get('format')
+        try:
+            field_format = OpenApiFormat(field_format)
+        except ValueError:
+            pass
+        match field_format:
+            case OpenApiFormat.BOOLEAN:
+                return ConfiguredBooleanField(field_metadata, is_required=is_required).field_instance
+            case OpenApiFormat.CHARACTER_VARYING | OpenApiFormat.UUID:
+                return ConfiguredCharField(field_metadata, is_required=is_required).field_instance
+            case OpenApiFormat.DATE:
+                return ConfiguredDateField(field_metadata, is_required=is_required).field_instance
+            case OpenApiFormat.INTEGER:
+                return ConfiguredIntegerField(field_metadata, is_required=is_required).field_instance
+            case _:
+                return DefaultConfiguredField(field_metadata, is_required=is_required).field_instance
