4a17ff3e57a5cb375b487f9084f1433cae19be8a
David Chan <davidchan125@hotmail.co.uk>
2025-08-01 14:28:35 +0100

    Override base Django date input template

--- gui/settings.py
+++ gui/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.forms',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gui.urls'
+
+FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 
 TEMPLATES = [
     {
