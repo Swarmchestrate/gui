75bfa3526224fc677cbcb7f097b855364cfd3bdd
David Chan <davidchan125@hotmail.co.uk>
2025-08-01 15:46:56 +0100

    Add server side validation handling for capacity reg form

--- gui/views.py
+++ gui/views.py
@@ -1,5 +1,9 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import FormView
+from http import HTTPStatus
 
 from .forms import NewCapacityForm
 
@@ -13,6 +17,20 @@ def index(request):
 class CapacityEditorFormView(FormView):
     template_name = 'new_capacity.html'
     form_class = NewCapacityForm
+    success_url = reverse_lazy('new_capacity')
+    
+    def form_invalid(self, form):
+        super().form_invalid(form)
+        return JsonResponse(
+            {
+                'feedback': json.loads(form.errors.as_json()),
+                'url': self.request.path,
+            },
+            status=HTTPStatus.BAD_REQUEST
+        )
+    
+    def form_valid(self, form):
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
--- static/new_capacity.js
+++ static/new_capacity.js
@@ -1,3 +1,4 @@
+const form = document.querySelector("#editor-form");
 const fields = document.querySelectorAll("input, textarea, select");
 
 
@@ -11,6 +12,51 @@ const validateField = (event) => {
     }
     return field.classList.remove("is-invalid");
 };
+
+function displayValidationMessages(validationMessages) {
+    if ("__all__" in validationMessages) {
+        const allErrorsList = document.querySelector("#all-errors-list");
+        for (const error of validationMessages.__all__) {
+            const errorItem = document.createElement("li");
+            errorItem.textContent = error.message;
+            allErrorsList.appendChild(errorItem);
+        }
+        allErrorsList.classList.remove("d-none");
+    }
+    for (const fieldName in validationMessages) {
+        if (fieldName === "__all__") continue;
+        const field = document.querySelector(`[name="${fieldName}"]`);
+        const feedback = document.querySelector(`#${field.id} ~ .invalid-feedback`);
+        const feedbackList = document.createElement("ul");
+        feedbackList.classList.add("mb-0", "p-0", "list-unstyled");
+        const fieldValidationMessages = validationMessages[fieldName];
+        for (const validationMessageData of fieldValidationMessages) {
+            const errorItem = document.createElement("li");
+            errorItem.textContent = validationMessageData.message;
+            feedbackList.appendChild(errorItem);
+        }
+        feedback.appendChild(feedbackList);
+        field.classList.add("is-invalid");
+    }
+}
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const data = new URLSearchParams();
+    for (const pair of new FormData(form)) {
+        data.append(pair[0], pair[1]);
+    }
+    const response = await fetch(form.action, {
+        method: "POST",
+        body: data,
+    });
+    if (response.ok) {
+        return window.location.href = response.url;
+    }
+    const responseData = await response.json();
+    const validationMessages = responseData.feedback || {};
+    displayValidationMessages(validationMessages);
+});
 
 window.addEventListener("DOMContentLoaded", () => {
     fields.forEach((field) => {
