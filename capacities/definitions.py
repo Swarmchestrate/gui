c879cac440c0bca87f0e2c1312adf62aa21739cb
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-09-19 09:15:46 +0100

    Update capacities/definitions.py - Add resource_type to list of auto_generated_field_names

--- capacities/definitions.py
+++ capacities/definitions.py
@@ -6,3 +6,10 @@ class CapacityUserSpecifiableOpenApiDefinition(UserSpecifiableOpenApiDefinition)
         super().__init__(openapi_spec)
         self.definition_name = 'capacity'
         self.id_field = 'capacity_id'
+
+    # Properties
+    @property
+    def auto_generated_field_names(self):
+        names = super().auto_generated_field_names
+        names.append('resource_type')
+        return names
