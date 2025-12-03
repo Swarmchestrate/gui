332020497cc2595405cc4e7e90ce05c2d86f9161
David Chan <davidchan125@hotmail.co.uk>
2025-12-03 10:56:51 +0000

    Start application_pref_resource_providers app

---
+++ application_pref_resource_providers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApplicationPrefResourceProvidersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'application_pref_resource_providers'
