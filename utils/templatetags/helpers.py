ca472023b9fca2769ca4ad1e55b32b641b8efcac
davidchan125@hotmail.co.uk <davidchan125@hotmail.co.uk>
2025-12-19 11:48:29 +0000

    Improve next/prev buttons in wizard

--- utils/templatetags/helpers.py
+++ utils/templatetags/helpers.py
@@ -34,3 +34,26 @@ def get_key_value_or_empty_dict(d: dict, key: str):
 @register.filter
 def convert_str_date(value: str):
     return parser.parse(value)
+
+
+# Credit: https://stackoverflow.com/a/32801096
+@register.filter
+def next(list_, current_index):
+    """Returns the next element of the list using the current index
+    if it exists. Otherwise returns an empty string.
+    """
+    try:
+        return list_[int(current_index) + 1]  # access the next element
+    except Exception:
+        return ""  # return empty string in case of exception
+
+
+@register.filter
+def previous(list_, current_index):
+    """Returns the previous element of the list using the current index
+    if it exists. Otherwise returns an empty string.
+    """
+    try:
+        return list_[int(current_index) - 1]  # access the next element
+    except Exception:
+        return ""  # return empty string in case of exception
