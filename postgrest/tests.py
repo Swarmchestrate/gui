55b7f9da72442b005b4179df66158121ad201934
chanyoda <davidchan125@hotmail.co.uk>
2026-05-01 18:14:28 +0100

    Implement dynamically-generated mock openapi spec for testing

--- postgrest/tests.py
+++ postgrest/tests.py
@@ -1,6 +1,8 @@
+import json
 from django.test import SimpleTestCase
 
 from .new_api import ApiClient, OpenApiSpecification
+from .mock_api import generate_mock_openapi_spec
 
 # Create your tests here.
 class ApiClientTestCase(SimpleTestCase):
@@ -10,3 +12,9 @@ class ApiClientTestCase(SimpleTestCase):
         api_client.initialise_openapi_spec()
         schema = api_client.openapi_spec
         self.assertIsInstance(schema, OpenApiSpecification)
+
+
+class MockDataTestCase(SimpleTestCase):
+    def test_mock_openapi_spec(self):
+        mock_openapi_spec = generate_mock_openapi_spec()
+        print("mock_openapi_spec", mock_openapi_spec)
