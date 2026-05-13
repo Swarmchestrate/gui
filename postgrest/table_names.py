a040acc92170b3d005ba6fb4054a2c0e9bf67c8e
chanyoda <davidchan125@hotmail.co.uk>
2026-05-13 14:55:15 +0100

    Add application_microservice table to TableNames enum

--- postgrest/table_names.py
+++ postgrest/table_names.py
@@ -3,6 +3,7 @@ from enum import StrEnum
 
 class TableNames(StrEnum):
     APPLICATION = "application"
+    APPLICATION_MICROSERVICE = "application_microservice"
     APPLICATION_NEW = "application_new"
     CAPACITY = "capacity"
     CAPACITY_NEW = "capacity_new"
