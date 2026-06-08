da39317b73abfac2dffc06174f9c479d15b57786
chanyoda <davidchan125@hotmail.co.uk>
2026-06-08 11:24:34 +0100

    Add inline validation to fk resource editors

--- editor/static/editor/foreign_key_resource_editor.js
+++ editor/static/editor/foreign_key_resource_editor.js
@@ -1,7 +1,11 @@
+import { EditorValidator } from "/static/editor/validation.js";
 import { loadOneToOneFieldSections } from "/static/editor/one_to_one_field_sections.js";
 import { loadOneToManyFieldSections } from "/static/editor/one_to_many_field_sections.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
+    const form = document.querySelector("#fk-resource-form");
+    const validator = new EditorValidator(form);
+    validator.setupInlineValidation();
     loadOneToOneFieldSections();
     loadOneToManyFieldSections();
     const tooltipTriggerElements = Array.from(
